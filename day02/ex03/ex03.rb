54a8c52af04e446d4b98aee4f8af1bc2e41d1e83
Mufaro <mufaro@wethinkcode.co.za>
2020-06-09 13:37:27 +0200

    50% complete

--- day02/ex03/ex03.rb
+++ day02/ex03/ex03.rb
@@ -1,11 +1,12 @@
 class Elem
-  attr_reader :tag, :content, :tag_type, :opt
+  attr_reader :tag, :content, :tag_type, :opt, :html_array
 
-  def initialize(name, content=false, type=false, opt=false)
+  def initialize(name, content=false, type="double", opt=false)
     @tag = name
     @content = content if content
-    @type = type if type
+    @tag_type = type
     @opt = opt if opt
+    @html_array = []
   end
 
   def check_current_content
@@ -31,22 +32,52 @@ class Elem
 
   end
 
-  def add_tag
-    
-    if @tag_type == simple || @opt != nil
-      
+  def add_simple_tag(elem)
+    inline_css = ''
+    if elem.opt != nil
+      elem.opt.each do |key, value|
+        inline_css = inline_css + "#{key}=#{value} "
+      end 
+      inline_css.rstrip!
     end
-    return "<#{tag} #{inline_css}/>"
+    return "<#{elem.tag} #{inline_css}/>"
   end
 
-  def to_s
-    html_text = ''
+  def add_double_tag(elem, action="open")
+    @html_array.push("<#{elem.tag}>\n") if action == "open"
+    @html_array.push("</#{elem.tag}>") if action == "close"
+  end
+
+  def html_txt(elem)
+    return add_simple_tag(elem) if elem.tag_type == "simple"
+    return elem.content if elem.content.class == Text
+    add_double_tag(elem, "open")
+    html_txt(elem.content.shift) if elem.content.class == Array
+    add_double_tag(elem, "close")
+  end
     
+   # content.each do |elem|
+   #   @html_array.push("<#{@tag}>\n#{elem.html_txt}\n</#{@tag}>")
+   # end
+   # puts html_array.inspect()
+   # return html_array
+   #end
+
+  def to_s
+    html_txt(self)
+    puts "#{@html_array.join()}"
   end
 end
 
 class Text
+  attr_reader :text_string
+
   def initialize(text_string)
-    puts text_string
+    @text_string = text_string
+    self.to_s
+  end
+
+  def to_s
+   puts  "#{@text_string}"
   end
 end
